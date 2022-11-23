3194ec3a45906f9da9f78ef9c32e745653d9076f
devansh7061 <devanshsaini67p@gmail.com>
2022-11-23 12:08:32 +0530

    Added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './App.css'
-
-import { agent } from './veramo/setup'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Issuer from './pages/Issuer';
+import Reciever from './pages/Reciever';
+import Verifier from './pages/Verfier';
+import Home from './pages/Home';
 function App() {
-  const [didDoc, setDidDoc] = useState<any>()
-
-  const resolve = async () => {
-    const doc = await agent.resolveDid({
-      didUrl: 'did:ethr:goerli:0x847c34B56c0203A193d3CD3B96b8915C044fFfC7',
-    })
-
-    setDidDoc(doc)
-  }
-
-  useEffect(() => {
-    resolve()
-  }, [])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <pre id="result">{didDoc && JSON.stringify(didDoc, null, 2)}</pre>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/issuer" element={<Issuer />}></Route>
+        <Route path="/verifier" element={<Verifier />}></Route>
+        <Route path="/reciever" element={<Reciever />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
